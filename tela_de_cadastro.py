7b71f3705938d2ccfb0aaaa7f50fa5061f469565
abemoreira <abesam200319@gmail.com>
2025-05-13 14:38:57 -0400

    fiz mudança

--- tela_de_cadastro.py
+++ tela_de_cadastro.py
@@ -3,7 +3,9 @@ from PyQt5.QtWidgets import (
     QLabel, QPushButton, 
 QHboxLayout, QVBoxLayout, QMessagebox)
 from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtCore import Qt
 
-
-
-                                          
+class TelaInicial(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitl
